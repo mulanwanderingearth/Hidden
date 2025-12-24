82299383ef654a3624a970363e19c3bb37ecf30d
Xin Li <happyxin331@icloud.com>
2025-12-23 18:16:34 -0800

    completed

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 def hidden(matrix, n):
     # Your implementation here!
-    pass
-
+    #initiate a new list, result = ""
+    #loop through the matrix, flatten the matrix to a list. flatten=[]
+    #loop through flaten, when index%n == 0, add this letter to result
+    # result = ""
+    # flatten = []
+    # for row in matrix:
+    #     for letter in row:
+    #         flatten.append(letter)
+    # for index  in range(len(flatten)):
+    #     if index % n == 0:
+    #         result += flatten[index]
+    # return result
+    flatten = (letter for row in matrix for letter in row)
+    return "".join( letter for i, letter in enumerate(flatten) if i % n == 0)
 matrix_1 = (
     ('u','e','r','e', ' ', 'e'),
     ('d', 'z', 'o', 'b', 'i', 'v'),
@@ -22,5 +34,6 @@ matrix_2 = (
 assert hidden(matrix_2, 17) == '💜'
 assert hidden(matrix_2, 1) == '💜'
 
+print(hidden(matrix_2, 5))
 print("All tests passed! Discuss time and space complexity if time remains!")
 
